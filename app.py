5c19854a1f0f728611d97e5c540c5083176fd568
Emre <emre.ozan@icareweb.com>
2022-04-30 20:47:20 +0200

    app engine

--- app.py
+++ app.py
@@ -11,31 +11,31 @@ model_cooing = "COOLING_MODEL_GCP"
 model_heating = "EMRE_HEATING_MODEL_GCP"
 
 
-app.route("/")
+app.route("/index")
 def hello():
     return "Welcome to Energy Consumption Application!!"
 
-@app.route('/index', methods=['GET', 'POST'])
-def index():
-    if request.method == 'GET':
-        return render_template('index.html')
-    elif request.method == 'POST':
-        new_data = [{'X1': float(request.form["relative_compactness"]), 
-                    'X2': float(request.form["surface_area"]), 
-                    'X3': float(request.form["wall_area"]), 
-                    'X4': float(request.form["roof_area"]), 
-                    'X5': float(request.form["overall_height"]), 
-                    'X6': float(request.form["orientation"]),
-                    'X7': float(request.form["glazing_area"]), 
-                    'X8': float(request.form["glazing_area_dist"])}]
-        heating_load = predict_json(gcp_project, model_heating, new_data, version="v1")
-        cooling_load = predict_json(gcp_project, model_cooing, new_data, version="v1")
-        return render_template('prediction.html', 
-                                new_data=new_data, 
-                                heating_load=round(heating_load[0]["predicted_Y1"][0], 1),
-                                cooling_load=round(cooling_load[0]["predicted_Y2"][0], 1))
-    else:
-        return render_template('index.html')
+# @app.route('/index', methods=['GET', 'POST'])
+# def index():
+#     if request.method == 'GET':
+#         return render_template('index.html')
+#     elif request.method == 'POST':
+#         new_data = [{'X1': float(request.form["relative_compactness"]), 
+#                     'X2': float(request.form["surface_area"]), 
+#                     'X3': float(request.form["wall_area"]), 
+#                     'X4': float(request.form["roof_area"]), 
+#                     'X5': float(request.form["overall_height"]), 
+#                     'X6': float(request.form["orientation"]),
+#                     'X7': float(request.form["glazing_area"]), 
+#                     'X8': float(request.form["glazing_area_dist"])}]
+#         heating_load = predict_json(gcp_project, model_heating, new_data, version="v1")
+#         cooling_load = predict_json(gcp_project, model_cooing, new_data, version="v1")
+#         return render_template('prediction.html', 
+#                                 new_data=new_data, 
+#                                 heating_load=round(heating_load[0]["predicted_Y1"][0], 1),
+#                                 cooling_load=round(cooling_load[0]["predicted_Y2"][0], 1))
+#     else:
+#         return render_template('index.html')
 
 
 if __name__ == '__main__':
